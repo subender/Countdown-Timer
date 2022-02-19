4278a1a5f46a187e2c4e55e228f54777d747d088
subender <subender.io@outlook.com>
2022-02-19 20:23:44 +0530

    implemented javascript and css code

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("test");
+//Elements Selection
+const daysEl = document.querySelector("#days");
+const hoursEl = document.querySelector("#hours");
+const minuteEl = document.querySelector("#minute");
+const secondsEl = document.querySelector("#seconds");
+
+//Countdown date (time untill this date.)
+const newYear = "1 jan 2023";
+
+// Function for calculating time.
+const countdown = function () {
+  const newYearDate = new Date(newYear);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearDate - currentDate) / 1000;
+  const seconds = Math.floor(totalSeconds) % 60;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+
+  // Converting values into a sring so that we could use padStart() funciton.
+
+  daysEl.innerHTML = (days + "").padStart(2, 0);
+  hoursEl.innerHTML = (hours + "").padStart(2, 0);
+  minuteEl.innerHTML = (minutes + "").padStart(2, 0);
+  secondsEl.innerHTML = (seconds + "").padStart(2, 0);
+};
+
+setInterval(countdown, 1000);
